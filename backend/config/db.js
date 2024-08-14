65b2d386488a1263cebf14d8a63ff652f7b828e5
Kumudh Ranasinghe <kumudh.ranasinghe@gmail.com>
2024-08-14 11:02:43 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kumudh:1234@cluster0.autwy5u.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('Your Mongodb uri here').then(()=>console.log("DB Connected"));
 }
